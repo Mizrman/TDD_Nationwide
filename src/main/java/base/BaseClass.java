274ea3b44df1c3945b0e84d6b5019611a2680cde
Mizrman <mdmzrmn@gmail.com>
2022-11-13 12:58:10 -0500

    Updated TDD FrameWork

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -12,8 +12,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Parameters;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.Status;
 import com.google.common.io.Files;
